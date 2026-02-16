3d30b09a46f81991357db6399bda5534d82f11f8
novice0106 <makihito64@gmail.com>
2026-02-16 23:57:30 +0900

    [Reactハンズオンラーニング] chap5

---
+++ Reactハンズオンラーニング/sandbox/chap5.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>React Examples</title>
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="https://unpkg.com/react@16.8.6/umd/react.development.js"></script>
+        <script src="https://unpkg.com/react-dom@16.8.6/umd/react-dom.development.js"></script>
+        <script src="https://unpkg.com/@babel/standalone/bel.min.js"></script>
+    </body>
+    <script type="text/babel">
+        
+    </script>
+</html>
