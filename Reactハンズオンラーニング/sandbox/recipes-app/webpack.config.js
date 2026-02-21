7000bd168ce95c507d9e4f528c7c43524cbcce1f
novice0106 <makihito64@gmail.com>
2026-02-21 21:08:56 +0900

    [React ハンズオンラーニング] chap5

---
+++ Reactハンズオンラーニング/sandbox/recipes-app/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist", "assets"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        use: "babel-loader",
+      },
+    ],
+  },
+};
