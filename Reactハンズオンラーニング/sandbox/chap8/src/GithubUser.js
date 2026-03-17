136a6b530512050d7fe72bb47b491ce478f15fae
novice0106 <makihito64@gmail.com>
2026-03-17 22:24:21 +0900

    [React ハンズオンラーニング] chap8

--- Reactハンズオンラーニング/sandbox/chap8/src/GithubUser.js
+++ Reactハンズオンラーニング/sandbox/chap8/src/GithubUser.js
@@ -1,10 +1,26 @@
 import { useEffect, useState } from "react";
 
+const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
 function GithubUser({ login }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location,
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
+    if (data && data.login === login) return;
     fetch(`https://api.github.com/users/${login}`)
       .then((response) => response.json())
       .then(setData)
