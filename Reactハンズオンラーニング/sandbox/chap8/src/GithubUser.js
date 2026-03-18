9218757200326bebf198ec74a2a005b47cc7eae7
novice0106 <makihito64@gmail.com>
2026-03-18 22:07:23 +0900

    [React ハンズオンラーニング] chap8

--- Reactハンズオンラーニング/sandbox/chap8/src/GithubUser.js
+++ Reactハンズオンラーニング/sandbox/chap8/src/GithubUser.js
@@ -1,35 +1,34 @@
 import { useEffect, useState } from "react";
 
-const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
-
 function GithubUser({ login }) {
-  const [data, setData] = useState(loadJSON(`user:${login}`));
-
-  useEffect(() => {
-    if (!data) return;
-    if (data.login === login) return;
-    const { name, avatar_url, location } = data;
-    saveJSON(`user:${login}`, {
-      name,
-      login,
-      avatar_url,
-      location,
-    });
-  }, [data]);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!login) return;
-    if (data && data.login === login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
+      .then((data) => data.json())
       .then(setData)
-      .catch(console.error);
+      .then(() => setLoading(false))
+      .catch(setError);
   }, [login]);
 
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (loading) return <h1>loading...</h1>;
+  if (!data) return null;
 
-  return null;
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
